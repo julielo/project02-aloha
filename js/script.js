45b03d3b98add5f54ff7a2967ea7351ad869b9d3
Nguyen Khoa <nguyenkhoa@gmail.com>
2017-01-22 10:37:52 -0800

    complete updates section

--- js/script.js
+++ js/script.js
@@ -7,7 +7,35 @@
          $('#count').html(count).css('color','red').css('border','0.5px solid red').css('width','1rem').css('border-radius','50%').css('height','1rem').css('text-align','center');
          
     });
+    $('#email').on('focus',function() {
+      //set to empty string
+       $(this).val('');
+    })
+      $('#updatesbutton').on('click', function() {
+        var email = $('#email').val();
+        if (email != ''){
+          var valid = isValidEmailAddress(email);
+          
+          if (valid) {
+            var message = 'Your email is ' + email + ' Thanks for subscribing';
+            var title = "Aloha Shop Message"
+            //alert('Your email is ' + email + ' Thanks for subscribing');
+            alert(title, message);
+          }
+           
+        }
+        else{
+          alert('You forgot to enter an email');
+        }
+
+      //console.log($('#email').val()  );
+  });
  });
+
+ function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
 
  function scrollNav() {
   $('.nav a').click(function(){  
@@ -27,18 +55,8 @@
 }
 scrollNav();
 
- // var myLi ='<li>New Line </li>';
- // $('ul').append(myLi);
-
- // $('article').children('.opening-line').siblings().css('color','red')
- // $('article').children(':last-child').css('color','blue')
- // $('.opening-line').next().css('color','black');
-
-//  function shoppingCount() {
-//     var count =0;
-//     $('button').on('click', function() {
-//          count++;
-//          $('#count').html(count).css('color','red').css('border','0.5px solid red').css('width','1rem').css('border-radius','50%').css('height','1rem').css('text-align','center');
-//     });
-//  });
-//  }
+// function subscribe() {
+//   $('#updatesbutton').on('click', function() {
+//       console.log($('#email').attr("value"));
+//   });
+// }  
