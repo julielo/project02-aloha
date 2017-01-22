3aed011f832d9336fa5231aec5deacd4dc7afb50
Nguyen Khoa <nguyenkhoa@gmail.com>
2017-01-22 07:17:00 -0800

    Add smooth scroll

--- js/script.js
+++ js/script.js
@@ -9,12 +9,30 @@
     });
  });
 
- var myLi ='<li>New Line </li>';
- $('ul').append(myLi);
+ function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    debugger;
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 4000);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
 
- $('article').children('.opening-line').siblings().css('color','red')
- $('article').children(':last-child').css('color','blue')
- $('.opening-line').next().css('color','black');
+ // var myLi ='<li>New Line </li>';
+ // $('ul').append(myLi);
+
+ // $('article').children('.opening-line').siblings().css('color','red')
+ // $('article').children(':last-child').css('color','blue')
+ // $('.opening-line').next().css('color','black');
 
 //  function shoppingCount() {
 //     var count =0;
